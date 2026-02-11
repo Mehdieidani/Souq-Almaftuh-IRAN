cf4ba50927d79e2d8cc6ca362a44a174c6bb5570
Mehdieidani <161817237+Mehdieidani@users.noreply.github.com>
2026-02-11 18:06:11 +0330

    Delete deepseek_bash_20260211_2ae108.sh

--- deepseek_bash_20260211_2ae108.sh
+++
@@ -1,6 +0,0 @@
-composer create-project laravel/laravel souq-iran
-cd souq-iran
-composer require laravel/sanctum laravel/reverb pusher/pusher-php-server filament/filament "livewire/livewire:^3.0" guzzlehttp/guzzle
-php artisan sanctum:install
-php artisan filament:install --panels
-npm install && npm run build
